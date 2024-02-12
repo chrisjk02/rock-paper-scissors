ca7989d6708ea8f25323f0b48cc1ce00c5982dfb
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-02-12 14:59:33 -0800

    Add if statements to determine what move to print to console using the random number generated.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
     //generate random number from 0-2 (inclusive)
+    const num = Math.floor(Math.random() * 3);
     //using generated number:
-    // if 0, rock
-    // if 1, return paper
-    // if 2, return scissors
+    if (num === 0) {
+        return 'Rock';
+    } else if (num === 1) {
+        return 'Paper';
+    } else if (num === 2) {
+        return 'Scissors';
+    } else {
+        return 'No valid number';
+    }
 }
 
 console.log(getComputerChoice());
