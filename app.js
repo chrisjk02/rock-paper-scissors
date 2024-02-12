deb17483dbc23b98081bea3dcd494bcc7507cd82
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-02-12 14:53:24 -0800

    Add pseudocode for generating computer move

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function getComputerChoice() {
-
+    //generate random number from 0-2 (inclusive)
+    //using generated number:
+    // if 0, rock
+    // if 1, return paper
+    // if 2, return scissors
 }
