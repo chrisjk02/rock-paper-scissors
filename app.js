686d99950b5849bd8a817af67503f29060d4593a
Chris K <133260386+chrisjk02@users.noreply.github.com>
2024-02-12 14:57:08 -0800

    Test random number generator

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
     //generate random number from 0-2 (inclusive)
     //using generated number:
     // if 0, rock
     // if 1, return paper
     // if 2, return scissors
 }
+
+console.log(getComputerChoice());
